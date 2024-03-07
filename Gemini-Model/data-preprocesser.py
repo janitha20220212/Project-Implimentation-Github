0ed08076fbd359670fe1b4dddbb5e2685547b4c2
Suseenthiran Arulraj Rumeasiyan <srumeasiyan@gmail.com>
2024-03-07 14:10:04 +0530

    imports re and pandas module

---
+++ Gemini-Model/data-preprocesser.py
@@ -0,0 +1,4 @@
+# imports re module which provides support for regular expressions to be used in the python program
+import re
+# imports the pandas module which is used for data manipulation and analysis
+import pandas as pd
