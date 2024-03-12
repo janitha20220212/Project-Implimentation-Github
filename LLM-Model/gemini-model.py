7e60912bdc87d45a4eed1ce432c29b95ecc47fae
Rumeasiyan <srumeasiyan@gmail.com>
2024-03-12 10:11:26 +0530

    changes llm structure and merge folder of llm and db

--- LLM-Model/gemini-model.py
+++ LLM-Model/gemini-model.py
@@ -9,33 +9,6 @@ import google.generativeai as genai
 
 genai.configure(api_key="AIzaSyAVXflbZLBt9XILv5om1PGgCYc5NHxDcbs")
 
-# Set up the model
-# generation_config = {
-#     "temperature": 0.9,
-#     "top_p": 1,
-#     "top_k": 1,
-#     "max_output_tokens": 2048,
-# }
-
-# safety_settings = [
-#     {
-#         "category": "HARM_CATEGORY_HARASSMENT",
-#         "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-#     },
-#     {
-#         "category": "HARM_CATEGORY_HATE_SPEECH",
-#         "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-#     },
-#     {
-#         "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-#         "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-#     },
-#     {
-#         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-#         "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-#     },
-# ]
-
 model = genai.GenerativeModel('gemini-pro')
 
 df = pd.read_csv('./LLM-Model/preprocessed_data.tsv', sep='\t')
