16f84177e7ded8fc8aed25cd4b8d2c03d05429af
Rumeasiyan <srumeasiyan@gmail.com>
2024-03-22 14:44:09 +0530

    adds flagpost deployed route

--- NPZ New/contentScript.js
+++ NPZ New/contentScript.js
@@ -342,7 +342,7 @@ function getData(et) {
     };
 
     // let response = fetch("http://127.0.0.1:5000/flagpost/", {
-    let response = fetch("https://nospoilerzone.azurewebsites.net/flagpost/", {
+        let response = fetch("https://nospoilerzone.azurewebsites.net/flagpost/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
