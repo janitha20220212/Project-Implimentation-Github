98e8ebbe5f2d8329f5fc8f71b3fb20aa4aaa0ca6
Rumeasiyan <srumeasiyan@gmail.com>
2024-03-24 16:36:37 +0530

    debugging the code for publishing extension

--- NPZ New/contentScript.js
+++ NPZ New/contentScript.js
@@ -181,22 +181,22 @@ async function checkForSpoilers() {
     var currentURL = window.location.href;
 
     if (currentURL.includes("reddit.com")) {
-        let loadingScreen;
-        loadingScreen = document.createElement("div");
-        loadingScreen.id = "loading-screen";
-        loadingScreen.style.position = "fixed";
-        loadingScreen.style.top = "0";
-        loadingScreen.style.left = "0";
-        loadingScreen.style.width = "100%";
-        loadingScreen.style.height = "100%";
-        loadingScreen.style.backgroundColor = "rgba(0, 0, 0)";
-        loadingScreen.style.justifyContent = "center";
-        loadingScreen.style.alignItems = "center";
-        loadingScreen.style.display = "flex";
-        loadingScreen.style.zIndex =
-            "1000000000000000000000000000000000000000000000000000000000";
-        loadingScreen.innerHTML =
-            "<h1 style='font-size: 3rem; color: white; text-align: center;'>Your Spoiler are being detected</h1>";
+        // let loadingScreen;
+        // loadingScreen = document.createElement("div");
+        // loadingScreen.id = "loading-screen";
+        // loadingScreen.style.position = "fixed";
+        // loadingScreen.style.top = "0";
+        // loadingScreen.style.left = "0";
+        // loadingScreen.style.width = "100%";
+        // loadingScreen.style.height = "100%";
+        // loadingScreen.style.backgroundColor = "rgba(0, 0, 0)";
+        // loadingScreen.style.justifyContent = "center";
+        // loadingScreen.style.alignItems = "center";
+        // loadingScreen.style.display = "flex";
+        // loadingScreen.style.zIndex =
+        //     "1000000000000000000000000000000000000000000000000000000000";
+        // loadingScreen.innerHTML =
+        //     "<h1 style='font-size: 3rem; color: white; text-align: center;'>Your Spoiler are being detected</h1>";
         // console.log("Reddit page");
         const articles = document.querySelectorAll(
             "article.w-full, faceplate-tracker"
@@ -256,7 +256,9 @@ async function checkForSpoilers() {
             let postUniqueLink = "no-link";
             postUniqueLink = hrefAttribute;
 
-            article.appendChild(loadingScreen);
+            // if (!article.classList.contains("already-checked")) {
+            //     article.appendChild(loadingScreen);
+            // }
 
             containsSpoiler = await fetchModel(
                 totalContent,
@@ -272,7 +274,7 @@ async function checkForSpoilers() {
                 hideSpoilerPosts(firstChild);
                 article.classList.add("contains-spoiler");
             }
-            article.removeChild(loadingScreen);
+            // article.removeChild(loadingScreen);
         }
     } else if (currentURL.includes("twitter.com")) {
         // console.log("Twitter page");
@@ -345,7 +347,9 @@ async function checkForSpoilers() {
 
                 // addPostLoading(article, loadingScreen);
                 // article.classList.add("loading-screen");
-                article.appendChild(loadingScreen);
+                if (!article.classList.contains("contains-spoiler")) {
+                    article.appendChild(loadingScreen);
+                }
 
                 containsSpoiler = await fetchModel(
                     totalContent,
